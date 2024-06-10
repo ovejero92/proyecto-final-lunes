78c98e0689b5f3f2f5bc25ecc47c50cda439dbe2
Gustavo Ovejero <gustavo.ovejero@educacion.local>
2024-06-10 19:53:11 -0300

    final

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,55 +1,102 @@
+const productos = [
+    {nombre:'Remera',precio:7526,stock:0,img:'remera-negra.jpeg',categoria:'Ropa'},
+    {nombre:'Pantalon',precio:14155,stock:20,img:'pantalon-shens.png',categoria:'Ropa'},
+    {nombre:'Cartera',precio:62910,stock:0,img:'cartera-marron.png',categoria:'Accesorios'},
+    {nombre:'Zapatillas',precio:35992,stock:20,img:'zapatilla-negra.jpg',categoria:'Ropa'},
+    {nombre:'Freidora',precio:224999,stock:20,img:'airFrier.png',categoria:'Electrodomesticos'},
+    {nombre:'Microondas',precio:234999,stock:0,img:'micro.jpeg',categoria:'Electrodomesticos'},
+    {nombre:'Camisa',precio:17607,stock:10,img:'remera-azul.jpeg',categoria:'Ropa'},
+    {nombre:'Lavarropas',precio:1099999,stock:10,img:'lavarropa.jpeg',categoria:'Electrodomesticos'},
+    {nombre:'Lujo',precio:24192,stock:0,img:'perfume-lujo.avif',categoria:'Perfumes'},
+    {nombre:'Pink',precio:31450,stock:20,img:'perfume-pink.png',categoria:'Perfumes'},
+    {nombre:'Belle',precio:104396,stock:110,img:'perfume-belle.jpeg',categoria:'Perfumes'},
+    {nombre:'Aislad',precio:43390,stock:20,img:'perfume-aislada.avif',categoria:'Perfumes'},
+];
+let carrito = [];
+let selector = document.getElementById('selectorProductos')
 
-let CP = document.getElementById('CP')
-
-// const productosLS = JSON.parse(localStorage.getItem('productos')) || productos
-// localStorage.setItem('productos',JSON.stringify(productos))
-
-// let carrito = JSON.parse(localStorage.getItem('carrito')) || []
-
-productos.forEach((prod)=>{
-    let card = document.createElement('div')
-    card.classList.add('card')
-
-    let divImg = document.createElement('div')
-    divImg.style.textAlign = 'center'
-    
-    let img = document.createElement('img')
-    img.setAttribute('src', prod.img)
-    img.style.width = '150px'
-    img.style.borderRadius = '15px'
-    
-    let h2 = document.createElement('h2')
-    h2.textContent = `nombre: ${prod.nombre}`
-
-    let p = document.createElement('p')
-    p.textContent = `precio: $${prod.precio}`
- 
-    let sotck = document.createElement('p')
-    sotck.textContent = `stock: ${prod.stock}`
-
-    let divBtn = document.createElement('div')
-    divBtn.innerHTML = `<button >Agregar al Carrito</>`
+function mostrarProductos(){
+    let container = document.getElementById('CP')
+    container.innerHTML = '';
+    productos.forEach( prod =>{
+    let CC = document.createElement('div')
+    CC.classList.add('card');
+    CC.innerHTML = `
+    <div style='text-align:center;'><img src="./assets/img/${prod.img}" alt="${prod.nombre}" style='width:150px;border-radius:15px'></div>
+    <h2>${prod.nombre}</h2>
+    <p>Precio: $${prod.precio.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}</p>
+    <p>Stock: ${prod.stock}<p>
+    <div><button onclick="agregarProductoAlCarrito('${prod.nombre}')" class='botonCart'>Agregar al carrito</button></div>
+    `
+    container.appendChild(CC)
 
     if(prod.stock == 0){
-        let imgST = document.createElement('img')
-        imgST.setAttribute('src','https://www.gakcreations.com.ar/productos/sold2.png')
-        imgST.style.width = '190px'
-        imgST.style.position = 'absolute'
-        imgST.style.top = '0'
-        imgST.style.right = '0'
-        
-        card.appendChild(imgST)
-        card.style.background = '#9a9494'
+    CC.innerHTML = `
+      <div style='text-align:center;'><img src="./assets/img/${prod.img}" alt="${prod.nombre}" style='width:150px;border-radius:15px'></div>
+      <h2>${prod.nombre}</h2>
+      <p>Precio: $${prod.precio.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}</p>
+      <p>Stock: ${prod.stock}<p>
+      <div><button onclick="alertaError('No hay mas ${prod.nombre} disponible')" class='botonCartError'>Agregar al carrito  </button></div>
+    `
+    let img = document.createElement('img')
+    img.setAttribute('src','./assets/img/sinStock.png')
+    img.style.width = '190px'
+    img.style.position = 'absolute'
+    img.style.top = '0'
+    img.style.right = '0'
+    CC.appendChild(img)
+    CC.style.background = '#9a9494'
     }
-    
-    CP.appendChild(card)
-    card.appendChild(divImg)
-    divImg.appendChild(img)
-    card.appendChild(h2)
-    card.appendChild(p)
-    card.appendChild(sotck)
-    card.appendChild(divBtn)
-})
+    const CU = obtenerCategoriasUnicas()
+    selector.innerHTML = ''
+    const optdis = document.createElement('option')
+    optdis.text = 'Categorias'
+    optdis.setAttribute('selected','')
+    optdis.setAttribute('disabled','')
+    CU.forEach(cat => {
+        const opt = document.createElement('option')
+        opt.text = cat;
+        opt.value = cat;
+        selector.appendChild(opt)
+    }) 
+    selector.appendChild(optdis)
+    })
+}
+
+function buscarProducto(nombre){
+    let productosFiltrados = productos.filter( p => {return p.nombre.toLowerCase().includes(nombre.toLowerCase())})
+    mostrarProductosFiltrados(productosFiltrados)
+}
+function buscarCategoria(categoria){
+    let productosCategoria = productos.filter(p => {return p.categoria.toLowerCase().includes(categoria.toLowerCase())})
+    mostrarProductosFiltrados(productosCategoria)
+}
+function mostrarProductosFiltrados(productosFiltrados){
+    let container = document.getElementById('CP');
+    container.innerHTML = '';
+    productosFiltrados.forEach(prod => {
+        let CC = document.createElement('div');
+        CC.classList.add('card')
+        CC.innerHTML = `
+        <div style='text-align:center;'><img src="./assets/img/${prod.img}" alt="${prod.nombre}" style='width:150px;border-radius:15px'></div>
+        <h2>${prod.nombre}</h2>
+        <p>Precio: $${prod.precio}</p>
+        <p>Stock: ${prod.stock}<p>
+        <div><button class='botonCart' onclick="agregarProductoAlCarrito('${prod.nombre}')">addCard</button></div>
+        `
+        container.appendChild(CC)
+        if(prod.stock == 0){
+            let img = document.createElement('img')
+            img.setAttribute('src','./assets/img/sinStock.png')
+            img.style.width = '190px'
+            img.style.position = 'absolute'
+            img.style.top = '0'
+            img.style.right = '0'
+            CC.appendChild(img)
+            CC.style.background = '#9a9494'
+        }
+    })
+}
 
 document.getElementById('carritoBoton').addEventListener('click',()=>{
     document.getElementById('carrito').style.display = 'block'
@@ -58,3 +105,61 @@ document.getElementById('carritoBoton').addEventListener('click',()=>{
 document.getElementById('cerrarCarrito').addEventListener('click',()=>{
     document.getElementById('carrito').style.display = 'none'
 })
+
+function agregarProductoAlCarrito(nombre){
+    const producto = productos.find(p => p.nombre === nombre)
+    const itemEnCarrito = carrito.find(i => i.producto.nombre === nombre)
+
+    if(itemEnCarrito){
+        if (itemEnCarrito.cantidad < producto.stock) {
+            itemEnCarrito.cantidad++;
+        } else{
+            alert('no hay suficioente stock')
+            itemEnCarrito.cantidad
+        }
+    } else {
+    carrito.push({producto, cantidad:1})
+    }
+    actualizarCarrito()
+}
+
+
+function actualizarCarrito(){
+    const cartItemsContainer = document.getElementById('cartItems')
+    cartItemsContainer.innerHTML = ''
+
+    carrito.forEach(item => {
+        const cartItemElement = document.createElement('div');
+        const contadorCarrito = document.getElementById('contadorCarrito')
+        cartItemElement.className = 'cart-item';
+        cartItemElement.innerHTML = `
+            <span>${item.producto.nombre}</span>
+            <span>${item.producto.precio} $</span>
+            <input type="number" value="${item.cantidad}" min="0" onchange="actualizarCantidad('${item.producto.nombre}', this.value)">
+            <button onclick="eliminarProducto('${item.producto.nombre}')" class='eliminarProducto' ><i class="fa-solid fa-trash"></i></button>
+        `;
+        contadorCarrito.textContent = carrito.length
+        cartItemsContainer.appendChild(cartItemElement);
+    })
+    document.getElementById('cartTotal').innerText = obtenerTotal()
+    document.getElementById('cartCount').innerText = carrito.reduce((count, item) => count + item.cantidad, 0);
+}
+
+function obtenerTotal(){
+    return carrito.reduce((total,item) => total + item.producto.precio * item.cantidad,0).toFixed(2)
+}
+function eliminarProducto(nombre){
+carrito = carrito.filter(i => i.producto.nombre !== nombre);
+actualizarCarrito()
+}
+function obtenerCategoriasUnicas(){
+    const categoriasUnicas = new Set()
+
+    productos.forEach(prod => {
+        categoriasUnicas.add(prod.categoria)
+    })
+
+    return Array.from(categoriasUnicas)
+}
+
+mostrarProductos()
